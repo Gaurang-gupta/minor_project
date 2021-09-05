018ff9651cd67c36b20defaba80bcbd61b887628
Gaurang gupta <g12172001@gmail.com>
2021-09-05 16:32:23 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -4,8 +4,8 @@ import Navbar from './Components/Navbar';
 import Library from './Components/Library';
 function App() {
   return (
-    <Router>
-      <div className="app">
+    <div className="app">
+      <Router>
         <Navbar/>
         <Switch>
           <Route path="/library">
@@ -14,13 +14,15 @@ function App() {
           <Route path="/contacts">
             <h1>I am contacts</h1>
           </Route>
+          <Route path="/softwares">
+            <h1>Softwares</h1>
+          </Route>
           <Route path="/">
             <h1>I am home</h1>
           </Route>
         </Switch>
-      </div>
-      
-    </Router>
+      </Router>
+    </div>
   );
 }
 

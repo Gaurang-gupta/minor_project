96987a39cae871b9b907afb7c0982e2eaa7dd95c
Gaurang gupta <g12172001@gmail.com>
2021-09-05 06:22:16 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import './App.css';
+import { BrowserRouter as Router, Switch, Route} from "react-router-dom";
+import Navbar from './Components/Navbar';
+
+function App() {
+  return (
+    <Router>
+      <div className="app">
+        <Navbar/>
+      <Switch>
+        <Route path="/contacts">
+          <h1>I am contacts</h1>
+        </Route>
+        <Route path="/">
+          <h1>I am home</h1>
+        </Route>
+      </Switch>
+      </div>
+      
+    </Router>
+  );
+}
+
+export default App;
---
+++ src/Components/Dropdown.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import { BrowserRouter as Router,Link } from "react-router-dom";
+import "./Dropdown.css"
+function Dropdown() {
+    return (
+        <Router>
+        <div className="dropDown">
+            <div className="dropDown__links">
+                <Link to="/contacts" className="link">Question Papers</Link>
+            </div>
+            <div className="dropDown__links">
+                <Link to="/contacts" className="link">Time Table</Link>
+            </div>
+            <div className="dropDown__links">
+                <Link to="/contacts" className="link">Quick Links</Link>
+            </div>
+            <div className="dropDown__links">
+                <Link to="/contacts" className="link">Forms and Fee Links</Link>
+            </div>
+            <div className="dropDown__links">
+                <Link to="/contacts" className="link">Softwares</Link>
+            </div>
+            <div className="dropDown__links">
+                <Link to="/emergency-contacts" className="link">Emergency Contacts</Link>
+            </div>
+        </div>
+        </Router>
+    )
+}
+
+export default Dropdown

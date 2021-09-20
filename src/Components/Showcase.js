6f93c9f0d34fbe1d697daa1691dbe2d5881a2a7c
Nimit Jain <nimitashokjain@gmail.com>
2021-09-20 12:52:40 +0530

    updated the name

--- src/Components/Showcase.js
+++ src/Components/Showcase.js
@@ -5,7 +5,7 @@ function Showcase() {
         <div className="showcase">
         <img class="video-feature--mobile-img" src="https://www.oberlin.edu/sites/default/files/styles/width_1600/public/content/home/header-video-cover-image/aerial-campus-wbradford-40.jpg?itok=OPGqFteY" alt="Aerial view of campus" />
         <div className="para"> 
-        <h1 className="pheading">Student council  welfare </h1> </div>
+        <h1 className="pheading"> NITJ Student welfare portal</h1> </div>
         </div>
     )
 }

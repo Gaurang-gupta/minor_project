dd11d1ebed62497491520772fe8bca6a53f0f005
Nimit Jain <nimitashokjain@gmail.com>
2021-09-21 17:35:06 +0530

    time table page is pushed

--- src/Components/Showcase.js
+++ src/Components/Showcase.js
@@ -3,16 +3,13 @@ import './Showcase.css';
 function Showcase() {
     return (
         <div className="showcase">
-<<<<<<< HEAD
         <img class="video-feature--mobile-img" src="https://www.oberlin.edu/sites/default/files/styles/width_1600/public/content/home/header-video-cover-image/aerial-campus-wbradford-40.jpg?itok=OPGqFteY" alt="Aerial view of campus" />
         <div className="para"> 
         <h1 className="pheading"> NITJ Student welfare portal</h1> </div>
-=======
-            <img class="video-feature--mobile-img" src="https://www.oberlin.edu/sites/default/files/styles/width_1600/public/content/home/header-video-cover-image/aerial-campus-wbradford-40.jpg?itok=OPGqFteY" alt="Aerial view of campus" />
+            {/* <img class="video-feature--mobile-img" src="https://www.oberlin.edu/sites/default/files/styles/width_1600/public/content/home/header-video-cover-image/aerial-campus-wbradford-40.jpg?itok=OPGqFteY" alt="Aerial view of campus" />
             <div className="para"> 
                 <h1 className="pheading">Student Council Welfare </h1> 
-            </div>
->>>>>>> 8ad27cd62b8858854ab85ade5d850a08bb206947
+            </div> */}
         </div>
     )
 }

1802b32c0b8365cb665bc41aab14756c9ca5b632
Nimit Jain <nimitashokjain@gmail.com>
2021-11-02 13:11:18 +0530

    added software page

--- src/softwares/IT.js
+++ src/softwares/IT.js
@@ -5,6 +5,68 @@ function IT({ branch }) {
         <div className="branch__name">
             <h1 className="branch__name__heading">{branch}</h1>
             <ol className="branch__name__list">
+                <li className="branch__name__listItem">
+                    <h2 className="branch__name__software__heading">C/C++ language</h2>
+                    <p>
+                        C++ is a general-purpose programming language created by Bjarne Stroustrup as an extension of the C programming language, or "C with Classes". The language has expanded significantly over time, and modern C++ now has object-oriented, generic, and functional features in addition to facilities for low-level memory manipulation. It is almost always implemented as a compiled language, and many vendors provide C++ compilers, including the Free Software Foundation, LLVM, Microsoft, Intel, Oracle, and IBM, so it is available on many platforms.
+                    </p>
+                </li>
+                <li className="branch__name__listItem">
+                    <h2 className="branch__name__software__heading">Java</h2>
+                    <p>
+                    Java is a general-purpose, class-based, object-oriented programming language designed for having lesser implementation dependencies. It is a computing platform for application development. Java is fast, secure, and reliable, therefore. It is widely used for developing Java applications in laptops, data centers, game consoles, scientific supercomputers, cell phones, etc.
+                    </p>
+                </li>
+                <li className="branch__name__listItem">
+                    <h2 className="branch__name__software__heading">Sublime Text/VS Code/Notepad+/Pycharm/Jupyter Notebook/DevC++</h2>
+                    <p>
+                    An integrated development environment (IDE) is a software application that provides comprehensive facilities to computer programmers for software development.
+                    An IDE allows developers to start programming new applications quickly because multiple utilities don’t need to be manually configured and integrated as part of the setup process. Developers also don’t need to spend hours individually learning how to use different tools.
+                    </p>
+                </li>
+                <li className="branch__name__listItem">
+                    <h2 className="branch__name__software__heading">HTML/CSS/Java Script</h2>
+                    <p>
+                   
+                    HTML provides the basic structure of sites, which is enhanced and modified by other technologies like CSS and JavaScript.
+                    CSS is used to control presentation, formatting, and layout.
+                    JavaScript is used to control the behavior of different elements.
+
+                    </p>
+                </li>
+                 <li className="branch__name__listItem">
+                    <h2 className="branch__name__software__heading">Linux</h2>
+                    <p>
+                    Just like Windows, iOS, and Mac OS, Linux is an operating system. In fact, one of the most popular platforms on the planet, Android, is powered by the Linux operating system. As an operating system, Linux is software that sits underneath all of the other software on a computer, receiving requests from those programs and relaying these requests to the computer’s hardware.
+                    </p>
+                </li>
+            <li className="branch__name__listItem">
+                    <h2 className="branch__name__software__heading">StarUML.</h2>
+                    <p>
+                    StarUML is an open source software modeling tool that supports the UML (Unified Modeling Language) framework for system and software modeling. It is based on UML version 1.4, provides eleven different types of diagram and it accepts UML 2.0 notation. It actively supports the MDA (Model Driven Architecture) approach by supporting the UML profile concept and allowing to generate code for multiple languages.
+                    </p>
+                </li>
+               
+                <li className="branch__name__listItem">
+                    <h2 className="branch__name__software__heading">MySQL</h2>
+                    <p>
+                    MySQL is a relational database management system (RDBMS) developed by Oracle that is based on structured query language (SQL).
+                    MySQL is one of the most recognizable technologies in the modern big data ecosystem. Often called the most popular database and currently enjoying widespread, effective use regardless of industry, it’s clear that anyone involved with enterprise data or general IT should at least aim for a basic familiarity of MySQL.
+                    </p>
+                </li>
+                <li className="branch__name__listItem">
+                    <h2 className="branch__name__software__heading">Cisco Network Assistant</h2>
+                    <p>
+                    Cisco Network Assistant simplifies wired and wireless network management for networks up to 80 devices with its intuitive GUI and a task-based menu. Cisco Network Assistant is free and is optimized to apply common services across Cisco switches, routers, wireless controllers, and access points. Through a direct link to Cisco Active Advisor you can view important information pertaining to the products on your network, such as Warranty and Contract Information, Advisories and End of Life notifications. Cisco Active Advisor will be automatically populated with the devices managed through Cisco Network Assistant.
+                    </p>
+                </li>
+                <li className="branch__name__listItem">
+                    <h2 className="branch__name__software__heading">Weka</h2>
+                    <p>
+                    Weka is a collection of machine learning algorithms for data mining tasks. The algorithms can either be applied directly to a dataset or called from your own Java code. Weka contains tools for data pre-processing, classification, regression, clustering, association rules, and visualization.
+                    </p>
+                </li>
+
 
             </ol>
         </div>

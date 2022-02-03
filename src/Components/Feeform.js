8cb9714c6f049b3e004e42aa6c6f657e404d26a5
Nimit Jain <nimitashokjain@gmail.com>
2022-02-03 20:34:42 +0530

    Updated form page and FAQs

--- src/Components/Feeform.js
+++ src/Components/Feeform.js
@@ -14,138 +14,76 @@ function Feeform() {
                     </a>
                 </li>
                 <li>
-                    <a href="https://www.nitj.ac.in/index.php/nitj_cinfo/pages/620">
-                        a to Deposit Hostel Allotment Fee for B.Tech (Batch-2020) Students
+                    <a href="https://www.nitj.ac.in/index.php/nitj_cinfo/pages/687">
+                    Regarding Semester Registration and Fee payment for M.Tech (Artificial Intelligence) Batch-2021 
                     </a>
                 </li>
                 <li>
 
-                    <a href="https://www.nitj.ac.in/index.php/nitj_cinfo/pages/611">
-                        Fee Structure and Instructions for Fee Payment and Semester Registration for B.Tech (Batch-2020)
-                    </a>
-                </li>
-                <li>
-                    <a href="https://www.nitj.ac.in/nitj_files/as/NDL_Registration_62490.pdf">
-                        Online Fee Payment for Minor Degree (Session July-December, 2021)
-                    </a>
-                </li>
-                <li>
                     <a href="https://www.nitj.ac.in/index.php/nitj_cinfo/pages/492">
-                        Online Registration and Fee payment Portal for PhD Students
+                    Online Fee Payment for Minor Degree (Session January-June, 2022) 
+                    </a>
+                </li>
+               
+                
+                <li>
+                    <a href="https://www.nitj.ac.in/index.php/nitj_cinfo/pages/675">
+                    Link for Online Fee Payment and Semester Registration (Session January-June, 2022) 
                     </a>
                 </li>
                 <li>
-                    <a href="https://www.nitj.ac.in/nitj_files/student_corner/fee_structure_of_odd_semester__for_the_academic_year_2021-22_21071957477.pdf">
-                        Fee Structure - Odd Semester (July, 2021) for Ongoing Batches
+                    <a href="https://www.nitj.ac.in/nitj_files/student_corner/Fee_structure_of_UG,PG,Ph_21121719226.pdf">
+                    Fee Structure of UG/PG & PhD for the Academic Year 2021-22 (Even Semester)
                     </a>
                 </li>
-                <li>
-                    <a href="https://www.nitj.ac.in/index.php/nitj_cinfo/pages/593">
-                    Online Portal for Fee Payment and Semester Registration (Except B.Tech Batch-2020)
-                    </a>
-                </li>
-                <li>
-                    <a href="https://www.nitj.ac.in/index.php/nitj_cinfo/pages/434">
-                    Online Payment for PhD / M.Tech Dissertation Submission Fee
-                    </a>
-                </li>
+                
                 <li>
                     <a href="https://www.nitj.ac.in/index.php/nitj_cinfo/pages/533">
-                    Notice regarding Payment of Mess Advance for the Even Semester of Academic Year 2020-21
+                    
+                    Notice regarding Payment of Mess Advance for the Even Semester of Academic Year 2021-22
                     </a>
                 </li>
-                <li>
-                    <a href="https://www.nitj.ac.in/index.php/nitj_cinfo/pages/592">
-                    Online Application for Refund of Institute Security Money for B.Tech (Batch 2017-2021) pass out Students
-                    </a>
-                </li>
-                <li>
-                    <a href="https://www.nitj.ac.in/index.php/nitj_cinfo/pages/536">
-                    Online Application for Refund of Institute Security Money for M.Tech, MBA and M.Sc. Batch 2018-2020 pass out Students
-                    </a>
-                </li>
-                <li>
-                    <a href="https://www.nitj.ac.in/nitj_files/as/Fee_structure_for_loan_and_scholarship_purpose_for_BTech_2020_2024_Batch_7158.pdf">
-                    Fee Structure for Loan and Scholarship purpose for B.Tech (New Admission 2020-21)
-                    </a>
-                </li>
-                <li>
-                    <a href="https://www.nitj.ac.in/nitj_files/student_corner/Fee_structure_for_loan_and_scholarship_purpose_for_MBA(SS)_20092939014.pdf">
-                    Fee Structure for Loan and Scholarship purpose of MBA (Self-Sponsored) (2020-2022 Batch)
-                    </a>
-                </li>
-                <li>
-                    <a href="https://www.nitj.ac.in/nitj_files/student_corner/Fee_structure_202020-21_(for_loan_purpose)_new_admission__2020-21_20092349628.pdf">
-                    Fee Structure for Loan and Scholarship purpose for UG/PG (New Admission 2020-21)
-                    </a>
-                </li>
-                <li>
-                    <a href="https://www.nitj.ac.in/nitj_files/as/Notice_-_for_Fee_Structure_for_Minor_Degree_Elective_AY_2020-21_22832.pdf">
-                    Fee Structure for Minor Degree Elective for the Academic Year 2020-21
-                    </a>
-                </li>
-                <li>
-                    <a href="https://www.nitj.ac.in/nitj_files/as/Revision_of_Course_fee_for_the_Summer-Winter_Contact_Courses_(1)_87105.pdf">
-                    Revision of Course fee for the Summer/Winter Contact Courses
-                    </a>
-                </li>
+                
+               
+              
             </ul>
 
         </div>
          <div className="feeform">
          <h1>Important Forms</h1>
          <ul className="feelist">
-             <li>
-                 <a href="https://www.nitj.ac.in/index.php/nitj_cinfo/pages/623">
-                 Online Registration Form for M.Tech and M.Sc. (Batch-2021) 
-                 </a>
-             </li>
-             <li>
-                 <a href="https://www.nitj.ac.in/index.php/nitj_cinfo/pages/608">
-                 Registration Form, Schedule and Seat Matrix for Branch Change of B.Tech (Batch-2020) 
-                 </a>
-             </li>
-             <li>
-
-                 <a href="https://www.nitj.ac.in//nitj_files/Admissions/MBA/Performa_seat_surrendered_21090334979.docx">
-                 Application Form for Surrender of Seat (M.Tech/M.Sc./MBA) 
-                 </a>
-             </li>
-             <li>
-                 <a href="https://www.nitj.ac.in/index.php/nitj_cinfo/pages/568">
-                 Online Registration Form for Additional Carry Subjects (Only for B.Tech Batch-2020) 
-                 </a>
-             </li>
-             <li>
-                 <a href="https://www.nitj.ac.in/index.php/nitj_cinfo/pages/609">
-                 Notice and Online Application Form for Minor Degree Elective for B.Tech 2nd year Students (Batch-2020) 
-                 </a>
-             </li>
-             <li>
-                 <a href="https://www.nitj.ac.in/index.php/nitj_cinfo/pages/43">
-                 Online Student Feedback Form for Subjects/Faculty (Session April - July 2021) for B.Tech 2nd Semester
-                 </a>
-             </li>
-             <li>
-                 <a href="https://www.nitj.ac.in/nitj_files/as/ETS_Form_-_Revised_Electronic_Thesis_Submission_Form_24_Oct_2020_(PDF)_13061.pdf">
-                 Revised Electronic Thesis Submission Form for PhD / M.Tech / M.Sc. / MBA
-                 </a>
-             </li>
+                <li>
+                    <a href="https://www.nitj.ac.in/index.php/nitj_cinfo/pages/522">
+                    Exam Result (Session December 2021) - B.Tech (Batch-2020), M.Tech, M.Sc., MBA (Batch-2021) 
+                    </a>
+                </li>
+                <li>
+                    <a href="https://www.nitj.ac.in/index.php/nitj_cinfo/pages/522">
+                    Exam Result (Session December 2021) - B.Tech (Batch-2018 and 2019), M.Tech, M.Sc., MBA (Batch-2020) 
+                    </a>
+                </li>
+                <li>
+                    <a href="https://www.nitj.ac.in/index.php/nitj_cinfo/pages/43">
+                    Online Student Feedback Form for Subjects/Faculty (Session July-December 2021)
+                    </a>
+                </li>
+                <li>
+                    <a href="https://www.nitj.ac.in/nitj_files/links/Notice_for_B_6512.pdf">
+                    Notice regarding Online Classes for B.Tech (Final year), M.Sc. and MBA Students
+                    </a>
+                </li>
+                <li>
+                    <a href="https://www.nitj.ac.in/nitj_files/links/Notice_for_M_29133.pdf">
+                    Notice regarding Online Classes for M.Tech and PhD Students
+                    </a>
+                </li>
+           
              <li>
                  <a href="https://www.nitj.ac.in/index.php/nitj_cinfo/pages/587">
                  IMPORTANT: COVID Vaccination Status Form
                  </a>
              </li>
-             <li>
-                 <a href="https://www.nitj.ac.in//nitj_files/Admissions/MTech/Seat_Surrender_Form_20092220875.pdf">
-                 Application Form for Surrender of Seat
-                 </a>
-             </li>
-             <li>
-                 <a href="https://www.nitj.ac.in/index.php/nitj_cinfo/pages/488">
-                 Notice and Application Form regarding Residents Students and Staff Welfare Fund (RSSWF)
-                 </a>
-             </li>
+            
              <li>
                  <a href="https://www.nitj.ac.in/index.php/nitj_cinfo/index/41">
                  Online Application Form for Transcripts and Other Certificates 

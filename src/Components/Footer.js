aef7da459f58be272927cb38c73c9b82ffa340fa
Nimit Jain <nimitashokjain@gmail.com>
2022-02-16 21:28:19 +0530

    changed the footer links

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -6,12 +6,12 @@ function Footer() {
     return (
         <div className="footer">
             <div className="footer__feedback">
-                <h2 className="footer__heading">Feedback</h2>
-                <p>Please give us your suggestions and feedback</p>
+                <h2 className="footer__heading">Join Us</h2>
+                {/* <p>Please give us your suggestions and feedback.</p> */}
                 <div className="footer__heading__links">
-                    <a className="footer__header__link" href="https://forms.gle/MU7mchfsLYZrWnqa6">  Click here for feedback form</a>
-                    <a className="footer__header__link" href="#">Click here for sending pdfs</a>
-                    <a className="footer__header__link" href="#">Privacy Policy</a>
+                    <a className="footer__header__link" href="https://forms.gle/MU7mchfsLYZrWnqa6">  Be a part of us!</a>
+                    <a className="footer__header__link" href="https://forms.gle/RWDaqoeenna7h5Yv5">Click here for sending pdfs</a>
+                    {/* <a className="footer__header__link" href="#">Privacy Policy</a> */}
                 </div>
             </div>
             <div className="footer__logo">
@@ -28,11 +28,12 @@ function Footer() {
                 <p>
                     <Link className='footer__link' to="/contactUs">Contact Us</Link>
                 </p>
-                <p>Follow Us</p>
+                <p>Mail Support</p>
                 <p className="footer__social">
-                    <FaInstagram className='instagram' />
+                    {/* <FaInstagram className='instagram' /> */}
                     <FaMailBulk className="social" />
-                    <FaTwitter className="social" />
+                    <p className='mail'>nitjswp@gmail.com</p>
+                    {/* <FaTwitter className="social" /> */}
                 </p>
                 <p>&#169; SWP</p>
                 <p>All rights reserved</p>
